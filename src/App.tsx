d9c249acae6bd01c8c0f77b422046743061567d3
Viplove Kale <viplovekale@gmail.com>
2023-10-30 14:45:17 +0530

    folder structured

---
+++ src/App.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import './globals.css';
+import {Routes, Route} from 'react-router-dom';
+import SigninForm from './_auth/forms/SigninForm';
+import SignupForm from './_auth/forms/SignupForm';
+import {Home} from './_root/pages';
+import AuthLayout from './_auth/AuthLayout'
+import RootLayout from './_root/RootLayout'
+
+
+const App = () => {
+  return (
+    <main className='flex h-screen'>
+     <Routes>
+        {/* Public ROUTES */}
+        <Route element={<AuthLayout/>}>
+            <Route path='/sign-in' element={<SigninForm/>} />
+            <Route path='/sign-up' element={<SignupForm/>} />
+        </Route>
+
+        {/* Priavte Routes */}
+        <Route element={<RootLayout/>}>
+         <Route index element={<Home />}/>
+        </Route>
+     </Routes>
+    </main>
+  )
+}
+
+export default App;
