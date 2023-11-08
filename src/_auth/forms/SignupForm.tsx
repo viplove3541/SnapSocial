4e0392db5ee872b681dad7a4bcbb68891c848a3a
Viplove Kale <viplovekale@gmail.com>
2023-11-08 09:54:28 +0530

    signup form

--- src/_auth/forms/SignupForm.tsx
+++ src/_auth/forms/SignupForm.tsx
@@ -1,12 +1,65 @@
-import React from 'react';
-import { Button } from "@/components/ui/button"
+import { Button } from "@/components/ui/button";
+import {
+  Form,FormControl,FormDescription,FormField,FormItem,FormLabel,FormMessage,} from "@/components/ui/form";
+import { Input } from "@/components/ui/input"
+import {useForm} from 'react-hook-form';
+import { zodResolver } from "@hookform/resolvers/zod";
+import { SignupValidation} from '@/lib/validation';
+import {z} from 'zod';
 
-const SigninForm = () => {
+
+
+
+const SignupnForm = () => {
+
+  // 1. Define your form.
+  const form = useForm<z.infer<typeof SignupValidation>>({
+    resolver: zodResolver(SignupValidation),
+    defaultValues: {
+      name: '',
+      username: '',
+      email: '',
+      password: '',
+    },
+  })
+ 
+  // 2. Define a submit handler.
+  function onSubmit(values: z.infer<typeof SignupValidation>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values)
+  }
+
   return (
-    <div>
-      <Button>click me</Button>
-    </div>
+       <Form {...form}>
+
+      <div className="sm:w-420 flex-center flex-col">
+         <img src="/assets/images/logo1.png" alt="logo" />
+         <h2 className="h3-bold md:h2-bold pt-5 sm:pt-12">Create a new account</h2>
+         <p className="text-light-3 small-medium md:base-regular mt-1">To use SnapSocial enter your details</p>
+      
+       <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <FormField
+          control={form.control}
+          name="username"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Username</FormLabel>
+              <FormControl>
+                <Input placeholder="shadcn" {...field} />
+              </FormControl>
+              <FormDescription>
+                This is your public display name.
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit">Submit</Button>
+      </form>
+      </div>
+    </Form>
   )
 }
 
-export default SigninForm;
+export default SignupnForm;
